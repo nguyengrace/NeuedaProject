596bb3991d756cea838ebef51fa53ca527568358
qquiche <ethanrbenson@gmail.com>
2024-06-11 13:52:17 -0500

    Converted demo to backend

--- backend/src/main/java/com/example/backend/repository/StockRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.backend.repository;
-import com.example.backend.model.Stock;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StockRepository extends JpaRepository<Stock, Long>{
-}
